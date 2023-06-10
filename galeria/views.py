8a6f1338c5f1dfceba260f883385564039c95e08
Edsson <dsson_aa@hotmail.com>
2023-06-10 00:13:57 -0300

    Iniciando projeto DJango

---
+++ galeria/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render 
+#'Render' é um metodo de renderizacao de pagina html. Vamos usar ela para renderizar as pags
+#Html que criarmos
+
+# Create your views here. As 'Views' sao entendidas aqui como pags html 
+
+
+def index(request):
+    return render(request, 'index.html')
