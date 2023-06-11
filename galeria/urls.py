a36b22e3f8e8eb5298df733b25ea3ef3a726a61e
Edsson <dsson_aa@hotmail.com>
2023-06-10 21:47:32 -0300

    Fiz muita coisa.... Mas neste ponto ja temos as pagina index.html linkada com os arquivos css e imagens. Tambem criei a pag imagem.html já acossiado com seus documentos estaticos (css e imagens) mas por algum motivo a pag index.html nao esta linkada a pag imagem.html

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
+
+#Aqui pode ser entendido como o codigo de direcionamento das paginas HTML do nosso projeto
+#Note que 
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('imagem/', imagem)
 ]
--- setup/settings.py
+++ setup/settings.py
@@ -126,8 +126,10 @@ STATIC_URL = 'static/'
 #Ao DJANGO que, todos os arquivos estaticos (aqui entendido como css) estão nesta pasta
 #Que vamos passar como parâmetro
 STATICFILES_DIRS = [
-
+    os.path.join(BASE_DIR, 'setup/static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
